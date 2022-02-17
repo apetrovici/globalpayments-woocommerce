807eaac9ae984af9a853f95b5c8180c7f4802ed2
Gonzalo Tieri <gonzalotieri@gmail.com>
2022-02-17 09:52:40 -0300

    TR-48: [WooCommerce]: changes from CR

--- assets/admin/js/globalpayments-enforce-single-gateway.js
+++ assets/admin/js/globalpayments-enforce-single-gateway.js
@@ -1,12 +1,12 @@
 ( function( $ ) {
     $( function() {
-        var digitalWallet = ['globalpayments_googlepay', 'globalpayments_applepay'];
+        var digitalWallet = [ 'globalpayments_googlepay', 'globalpayments_applepay' ];
         // Toggle GlobalPayments gateway on/off.
         $( '[data-gateway_id^="globalpayments_"]' ).on( 'click', '.wc-payment-gateway-method-toggle-enabled', function() {
             var toggle = true;
 
-            if( digitalWallet.includes( $( this ).closest('tr').data('gateway_id') ) ){
-                return ;
+            if ( digitalWallet.includes( $( this ).closest( 'tr' ).data( 'gateway_id' ) ) ) {
+                return;
             }
 
             // Toggle off
@@ -15,17 +15,16 @@
             }
 
             // Toggle on
-            var clicked = $( this ).closest('tr').data('gateway_id');
+            var clicked = $( this ).closest( 'tr' ).data( 'gateway_id' );
             $( '[data-gateway_id^="globalpayments_"]' ).each( function() {
                 if ( ! $( this ).find( 'span.woocommerce-input-toggle--disabled' ).length > 0 ) {
-                    if( !digitalWallet.includes( $( this ).data('gateway_id') ) && !digitalWallet.includes( clicked ) ) {
+                    if ( ! digitalWallet.includes( $( this ).data( 'gateway_id' ) ) && ! digitalWallet.includes( clicked ) ) {
                         var gateway_title = $( this ).closest( 'tr' ).find( 'a.wc-payment-gateway-method-title' ).text();
                         window.alert( 'You can enable only one GlobalPayments gateway at a time. ' +
                             'Please disable ' + gateway_title + ' first!' );
                         toggle = false;
                         return;
                     }
-                    
                 }
             });
 
