d0d9b5800cba10a8931f09c8ba23e521cf1e8022
apetrovici <adina.petrovici@endava.com>
2022-11-09 11:37:17 +0200

    change format

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -1,1 +1,40 @@
-<?phpnamespace GlobalPayments\WooCommercePaymentGatewayProvider\Utils;class Utils {	/**	 * Validate BNPL notification request.	 *	 * @return bool	 * @throws \Exception	 */	public static function validate_bnpl_request() {		if ( ! isset( $_SERVER['REQUEST_METHOD'] ) ) {			throw new \Exception( __( 'The request method is missing.', 'globalpayments-gateway-provider-for-woocommerce' ) );		}		if ( 'GET' !== strtoupper( $_SERVER['REQUEST_METHOD'] ) ) {			throw new \Exception( __( 'This request method is not supported.', 'globalpayments-gateway-provider-for-woocommerce' ) );		}		if ( ! self::verify_xgp_signature() ) {			throw new \Exception( __( 'Unknown signature.', 'globalpayments-gateway-provider-for-woocommerce' ) );		}		if ( empty( $_GET['id'] ) ) {			throw new \Exception( __( 'Missing transaction id.', 'globalpayments-gateway-provider-for-woocommerce' ) );		}		return true;	}	public static function verify_xgp_signature() {		if ( empty( $_GET['X-GP-Signature'] ) ) {			return false;		}		// @TODO: validate X-GP-Signature		return true;	}}
+<?php
+
+namespace GlobalPayments\WooCommercePaymentGatewayProvider\Utils;
+
+class Utils {
+	/**
+	 * Validate BNPL notification request.
+	 *
+	 * @return bool
+	 * @throws \Exception
+	 */
+	public static function validate_bnpl_request() {
+		if ( ! isset( $_SERVER['REQUEST_METHOD'] ) ) {
+			throw new \Exception( __( 'The request method is missing.', 'globalpayments-gateway-provider-for-woocommerce' ) );
+		}
+
+		if ( 'GET' !== strtoupper( $_SERVER['REQUEST_METHOD'] ) ) {
+			throw new \Exception( __( 'This request method is not supported.', 'globalpayments-gateway-provider-for-woocommerce' ) );
+		}
+
+		if ( ! self::verify_xgp_signature() ) {
+			throw new \Exception( __( 'Unknown signature.', 'globalpayments-gateway-provider-for-woocommerce' ) );
+		}
+
+		if ( empty( $_GET['id'] ) ) {
+			throw new \Exception( __( 'Missing transaction id.', 'globalpayments-gateway-provider-for-woocommerce' ) );
+		}
+
+		return true;
+	}
+
+	public static function verify_xgp_signature() {
+		if ( empty( $_GET['X-GP-Signature'] ) ) {
+			return false;
+		}
+
+		// @TODO: validate X-GP-Signature
+		return true;
+	}
+}
