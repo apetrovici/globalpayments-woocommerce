0def18a3b5f657fa400501f1333dc0b05f10694b
apetrovici <adina.petrovici@endava.com>
2023-02-01 14:06:11 +0200

    TR-68: bug fix back button

--- assets/frontend/js/globalpayments-bnpl.js
+++ assets/frontend/js/globalpayments-bnpl.js
@@ -1,1 +1,55 @@
-( function ( $ ) {	function GlobalPaymentsBNPLWooCommerce() {		this.attachEventHandlers();	};	GlobalPaymentsBNPLWooCommerce.prototype = {		/**		 * Add important event handlers for controlling the payment experience during checkout		 *		 * @returns		 */		attachEventHandlers: function () {			var self = this;			// Fix `Checkout` and `Order Pay` pages after back button			$( window ).on( 'pageshow' , function( e ) {				if ( e.originalEvent.persisted ) {					$( document.body ).on( 'wc_fragments_ajax_error', function () {						$( document.body ).trigger( 'wc_fragment_refresh' );					} );					$( document.body ).on( 'wc_fragments_refreshed', function () {						$( self.getForm() ).unblock();					} );				}			} );		},		/**		 * Gets the current checkout form		 *		 * @returns {Element}		 */		getForm: function () {			var checkoutForms = [				// Order Pay				'form#order_review',				// Checkout				'form[name="checkout"]',				// Add payment method				'form#add_payment_method',				// Admin Order Pay				'form#wc-globalpayments-pay-order-form',			];			var forms = document.querySelectorAll( checkoutForms.join( ',' ) );			return forms.item( 0 );		},	};	if ( ! window.GlobalPaymentsBNPLWooCommerce ) {		window.GlobalPaymentsBNPLWooCommerce = new GlobalPaymentsBNPLWooCommerce();	}} (	( window ).jQuery) );
+( function ( $ ) {
+	function GlobalPaymentsBNPLWooCommerce() {
+		this.attachEventHandlers();
+	};
+
+	GlobalPaymentsBNPLWooCommerce.prototype = {
+		/**
+		 * Add important event handlers for controlling the payment experience during checkout
+		 *
+		 * @returns
+		 */
+		attachEventHandlers: function () {
+			var self = this;
+
+			// Fix `Checkout` and `Order Pay` pages after back button
+			$( window ).on( 'pageshow' , function( e ) {
+				if ( e.originalEvent.persisted ) {
+					$( document.body ).on( 'wc_fragments_ajax_error', function () {
+						$( document.body ).trigger( 'wc_fragment_refresh' );
+					} );
+					$( document.body ).on( 'wc_fragments_refreshed', function () {
+						$( self.getForm() ).unblock();
+					} );
+				}
+			} );
+		},
+
+		/**
+		 * Gets the current checkout form
+		 *
+		 * @returns {Element}
+		 */
+		getForm: function () {
+			var checkoutForms = [
+				// Order Pay
+				'form#order_review',
+				// Checkout
+				'form[name="checkout"]',
+				// Add payment method
+				'form#add_payment_method',
+				// Admin Order Pay
+				'form#wc-globalpayments-pay-order-form',
+			];
+			var forms = document.querySelectorAll( checkoutForms.join( ',' ) );
+
+			return forms.item( 0 );
+		},
+	};
+
+	if ( ! window.GlobalPaymentsBNPLWooCommerce ) {
+		window.GlobalPaymentsBNPLWooCommerce = new GlobalPaymentsBNPLWooCommerce();
+	}
+} (
+	( window ).jQuery
+) );
