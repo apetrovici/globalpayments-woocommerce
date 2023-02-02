171dddbcdac4b0fdb2f632811199c40398d33bf8
apetrovici <adina.petrovici@endava.com>
2023-02-02 10:28:33 +0200

    TR-68: bug fix back button

--- assets/frontend/js/globalpayments-bnpl.js
+++ assets/frontend/js/globalpayments-bnpl.js
@@ -10,19 +10,24 @@
 		 * @returns
 		 */
 		attachEventHandlers: function () {
-			var self = this;
-
 			// Fix `Checkout` and `Order Pay` pages after back button
-			$( window ).on( 'pageshow' , function( e ) {
-				if ( e.originalEvent.persisted ) {
-					$( document.body ).on( 'wc_fragments_ajax_error', function () {
-						$( document.body ).trigger( 'wc_fragment_refresh' );
-					} );
-					$( document.body ).on( 'wc_fragments_refreshed', function () {
-						$( self.getForm() ).unblock();
-					} );
-				}
-			} );
+			$( window ).on( 'pageshow' , this.unblockForm.bind( this ) );
+		},
+
+		/**
+		 * Unblock the form on `Checkout` and `Order Pay` pages after back button
+		 *
+		 * @param e
+		 */
+		unblockForm: function ( e ) {
+			if ( e.originalEvent.persisted ) {
+				var $form = $( this.getForm() );
+				$( document.body ).on( 'wc_fragments_refreshed wc_fragments_ajax_error', function () {
+					if ( $form.is( '.processing' ) ) {
+						$form.removeClass( 'processing' ).unblock();
+					}
+				} );
+			}
 		},
 
 		/**
@@ -36,10 +41,6 @@
 				'form#order_review',
 				// Checkout
 				'form[name="checkout"]',
-				// Add payment method
-				'form#add_payment_method',
-				// Admin Order Pay
-				'form#wc-globalpayments-pay-order-form',
 			];
 			var forms = document.querySelectorAll( checkoutForms.join( ',' ) );
 
